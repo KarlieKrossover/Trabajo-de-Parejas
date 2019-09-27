e100f29b49f9cc3345aab59fe417709b731e4d42
Karlie Krossover <karlykidicaruskingdomhearts@gmail.com>
2019-09-27 10:09:39 -0500

    anotando datos del js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//hola
+//hay una función para conocer cuantos pixeles se ha scrolleado. Siempre se inicia desde el pixel 0.
+//necesitamos 2 datos: que tan lejos está el video de algo
+//window = la ventana del navegador. Variable global
+//eventListener: quiero que hagas algo cuando pase otra cosa
+//target: un elemento que hemos creado
+//ofsettop= los pixeles que hay desde el principio del documento hasta algo
+//ofsetparent = el padre del video
+
+window.addEventListener('scroll', function(){
+    var video = document.getElementById("rectangulo");
+    var posVideo = video.offsetTop;
+});
