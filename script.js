32522cbaba71a76a98ae70797a9edebd43785f88
anaamof <anaaof@icloud.com>
2019-09-27 10:46:59 -0500

    js con errores :/

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//hola
+window.addEventListener('scroll', function(){
+    var video = document.getElementById("video");
+    var posVideo = video.offsetTop;
+    var alturaVideo = videp.offsetHeight;
+    var currentScroll = window.pageYOffset;
+    var windowHeight = window.innerHeight;
+
+    if(posVideo + alturaVideo < currentScroll + windowHeight && posVideo > currentScroll){
+       video.classList.add ("play");
+    }else{
+        video.classList.remove("play");
+    }
+
+   // console.log("posVideo", posVideo);
+    //console.log("currentScroll", currentScroll);
+    //console.log("windowHeight", windowHeight);
+});
